c05475b400265e66d5f5e14d14863446befa9164
mookp12 <kraiviwat.p@gmail.com>
2025-08-11 15:11:50 +0700

    feat: integrate Tailwind CSS and add MovieCard component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
+
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
         {/* Render Movie Lists Here */}
+        <MovieCard />
       </section>
     </div>
   );
