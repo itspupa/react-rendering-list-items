313057acd6da3a8d9f50a5f216bf934f37de63cd
mookp12 <kraiviwat.p@gmail.com>
2025-08-11 15:17:10 +0700

    Revert "feat: integrate Tailwind CSS and add MovieCard component with styling"
    This reverts commit c05475b400265e66d5f5e14d14863446befa9164.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
-
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
         {/* Render Movie Lists Here */}
-        <MovieCard />
       </section>
     </div>
   );
